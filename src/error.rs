47a493ae70ae14b51fb0f8aa141b5432ff71e994
Masaharu TASHIRO <masatsr.kit@gmail.com>
2020-05-16 18:11:51 +0900

    [ADD] from failure to thiserror

--- src/error.rs
+++ src/error.rs
@@ -1,60 +1,14 @@
-use failure::{Backtrace, Context, Fail};
-use std::fmt;
-use std::fmt::Display;
+use thiserror::Error;
 
-#[derive(Debug)]
-pub struct Error {
-    inner: Context<ErrorKind>,
-}
-
-#[derive(Copy, Clone, Eq, PartialEq, Debug, Fail)]
-pub enum ErrorKind {
-    #[fail(display = "None error")]
+//#[derive(Copy, Clone, Eq, PartialEq, Debug, Fail)]
+#[derive(Error, Debug, Eq, PartialEq)]
+pub enum Error {
+    #[error("None error")]
     Optional,
-    #[fail(display = "Line Algebra error: Equations have no solutions")]
+    #[error("Line Algebra error: Equations have no solutions")]
     LinalgSolveNoSolutions,
-    #[fail(display = "Line Algebra error: Equations have infinite solutions")]
+    #[error("Line Algebra error: Equations have infinite solutions")]
     LinalgSolveInfSolutions,
-    #[fail(display = "Fitting error")]
+    #[error("Fitting error")]
     Fitting,
-}
-
-impl Fail for Error {
-    fn cause(&self) -> Option<&dyn Fail> {
-        self.inner.cause()
-    }
-
-    fn backtrace(&self) -> Option<&Backtrace> {
-        self.inner.backtrace()
-    }
-}
-
-impl Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        Display::fmt(&self.inner, f)
-    }
-}
-
-impl Error {
-    pub fn new(inner: Context<ErrorKind>) -> Error {
-        Error { inner }
-    }
-
-    pub fn kind(&self) -> &ErrorKind {
-        self.inner.get_context()
-    }
-}
-
-impl From<ErrorKind> for Error {
-    fn from(kind: ErrorKind) -> Error {
-        Error {
-            inner: Context::new(kind),
-        }
-    }
-}
-
-impl From<Context<ErrorKind>> for Error {
-    fn from(inner: Context<ErrorKind>) -> Error {
-        Error { inner }
-    }
 }
