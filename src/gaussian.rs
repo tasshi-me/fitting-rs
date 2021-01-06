9b8d494187bc9135512c234916970bd4a903106a
Masaharu TASHIRO <masatsr.kit@gmail.com>
2021-01-07 04:10:05 +0900

    feat: functions take an argument of generic type Float (C-GENERIC)
    https://rust-lang.github.io/api-guidelines/flexibility.html#functions-minimize-assumptions-about-parameters-by-using-generics-c-generic

--- src/gaussian.rs
+++ src/gaussian.rs
@@ -1,7 +1,7 @@
 use crate::error::Error;
-use crate::linalg::solve;
+use crate::linalg;
+use crate::linalg::Float;
 use ndarray::{array, Array1};
-use std::f64;
 
 /// Estimates the parameters of gaussian function for generic data.
 /// The return value is `(mu, sigma, a)`
@@ -29,7 +29,7 @@ use std::f64;
 ///
 /// # References
 /// [1] [E. Pastuchov ́a and M. Z ́akopˇcan, ”Comparison of Algorithms for Fitting a Gaussian Function used in Testing Smart Sensors”, Journal of Electrical Engineering, vol. 66, no. 3, pp. 178-181, 2015.](https://www.researchgate.net/publication/281907940_Comparison_of_Algorithms_For_Fitting_a_Gaussian_Function_Used_in_Testing_Smart_Sensors)
-pub fn fit(x_vec: Array1<f64>, y_vec: Array1<f64>) -> Result<(f64, f64, f64), Error> {
+pub fn fit<F: Float>(x_vec: Array1<F>, y_vec: Array1<F>) -> Result<(F, F, F), Error> {
     Ok(guos(x_vec, y_vec)?)
 }
 
@@ -72,14 +72,14 @@ pub fn fit(x_vec: Array1<f64>, y_vec: Array1<f64>) -> Result<(f64, f64, f64), Er
 /// ];
 /// assert_abs_diff_eq!(&y_vec, &expected_ans, epsilon = 1e-9);
 /// ```
-pub fn val(x: f64, mu: f64, sigma: f64, a: f64) -> f64 {
-    a * (-(x - mu).powi(2) / (2. * sigma.powi(2))).exp()
+pub fn val<F: Float>(x: F, mu: F, sigma: F, a: F) -> F {
+    a * (-(x - mu).powi(2) / (F::from(2).unwrap() * sigma.powi(2))).exp()
 }
 
 #[allow(dead_code)]
-fn caruanas(x_vec: Array1<f64>, y_vec: Array1<f64>) -> Result<(f64, f64, f64), Error> {
-    let len_x_vec = x_vec.len() as f64;
-    let sum_x = x_vec.iter().sum();
+fn caruanas<F: Float>(x_vec: Array1<F>, y_vec: Array1<F>) -> Result<(F, F, F), Error> {
+    let len_x_vec = F::from(x_vec.len()).ok_or(Error::Optional)?;
+    let sum_x = x_vec.sum();
     let sum_x_pow2 = x_vec.iter().map(|x| x.powi(2)).sum();
     let sum_x_pow3 = x_vec.iter().map(|x| x.powi(3)).sum();
     let sum_x_pow4 = x_vec.iter().map(|x| x.powi(4)).sum();
@@ -93,7 +93,7 @@ fn caruanas(x_vec: Array1<f64>, y_vec: Array1<f64>) -> Result<(f64, f64, f64), E
     let sum_x_log_y = y_vec
         .iter()
         .zip(x_vec.iter())
-        .map(|(y, x)| y.ln() * x)
+        .map(|(y, x)| y.ln() * *x)
         .sum();
     let sum_x_pow2_log_y = y_vec
         .iter()
@@ -102,22 +102,22 @@ fn caruanas(x_vec: Array1<f64>, y_vec: Array1<f64>) -> Result<(f64, f64, f64), E
         .sum();
     let b = array![sum_log_y, sum_x_log_y, sum_x_pow2_log_y];
 
-    let ans_x = solve(a, b)?;
+    let ans_x = linalg::solve(a, b)?;
     let (a, b, c) = (ans_x[0], ans_x[1], ans_x[2]);
 
-    let mu = -b / (2. * c);
-    let sigma = (-1. / (2. * c)).sqrt();
-    let a = (a - (b.powi(2) / (4. * c))).exp();
+    let mu = -b / (F::from(2).unwrap() * c);
+    let sigma = (-F::one() / (F::from(2).unwrap() * c)).sqrt();
+    let a = (a - (b.powi(2) / (F::from(4).unwrap() * c))).exp();
 
     Ok((mu, sigma, a))
 }
 
-fn guos(x_vec: Array1<f64>, y_vec: Array1<f64>) -> Result<(f64, f64, f64), Error> {
-    let sum_y_pow2 = y_vec.iter().map(|y| y.powi(2)).sum();
+fn guos<F: Float>(x_vec: Array1<F>, y_vec: Array1<F>) -> Result<(F, F, F), Error> {
+    let sum_y_pow2: F = y_vec.iter().map(|y| y.powi(2)).sum();
     let sum_x_y_pow2 = y_vec
         .iter()
         .zip(x_vec.iter())
-        .map(|(y, x)| x * y.powi(2))
+        .map(|(y, x)| *x * y.powi(2))
         .sum();
     let sum_x_pow2_y_pow2 = y_vec
         .iter()
@@ -141,11 +141,11 @@ fn guos(x_vec: Array1<f64>, y_vec: Array1<f64>) -> Result<(f64, f64, f64), Error
         [sum_x_pow2_y_pow2, sum_x_pow3_y_pow2, sum_x_pow4_y_pow2],
     ];
 
-    let sum_y_pow2_log_y = y_vec.iter().map(|y| y.powi(2) * y.ln()).sum();
+    let sum_y_pow2_log_y: F = y_vec.iter().map(|y| y.powi(2) * y.ln()).sum();
     let sum_x_y_pow2_log_y = y_vec
         .iter()
         .zip(x_vec.iter())
-        .map(|(y, x)| x * y.powi(2) * y.ln())
+        .map(|(y, x)| *x * y.powi(2) * y.ln())
         .sum();
     let sum_x_pow2_y_pow2_log_y = y_vec
         .iter()
@@ -158,12 +158,12 @@ fn guos(x_vec: Array1<f64>, y_vec: Array1<f64>) -> Result<(f64, f64, f64), Error
         sum_x_pow2_y_pow2_log_y,
     ];
 
-    let ans_x = solve(a, b)?;
+    let ans_x = linalg::solve(a, b)?;
     let (a, b, c) = (ans_x[0], ans_x[1], ans_x[2]);
 
-    let mu = -b / (2. * c);
-    let sigma = (-1. / (2. * c)).sqrt();
-    let a = (a - (b.powi(2) / (4. * c))).exp();
+    let mu = -b / (F::from(2).unwrap() * c);
+    let sigma = ((-F::one() / (F::from(2).unwrap() * c)) as F).sqrt();
+    let a = ((a - (b.powi(2) / (F::from(4).unwrap() * c))) as F).exp();
 
     Ok((mu, sigma, a))
 }
